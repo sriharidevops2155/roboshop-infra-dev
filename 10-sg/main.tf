88da438f33d019b1a8a9eb00ab3b3b8a220e7af5
sriharibandi <shreharii@gmail.com>
2025-06-24 23:23:17 +0530

    roboshop-infra-dev

---
+++ 10-sg/main.tf
@@ -0,0 +1,9 @@
+module "frontend" {
+    source = "../../terraform-aws-sg"
+    project = var.project
+    environment = var.environment
+    sg_name = var.frontend_sg_name
+    sg_description = var.frontend_sg_description
+    vpc_id = local.vpc_id
+}
+
