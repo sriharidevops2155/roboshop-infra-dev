9a0b4aa593451d61837b22bc4700116f9d479ef4
sriharibandi <shreharii@gmail.com>
2025-07-07 22:04:31 +0530

    roboshop-infrs-dev

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,12 @@ resource "aws_instance" "bastion" {
   instance_type = "t2.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
+  user_data = <<EOF
+    #!/bin/bash
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+    sudo yum -y install terraform
+  EOF
 
   tags = merge(
     local.common_tags,
